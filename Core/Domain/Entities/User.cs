d830ac6efd1c546389370e0cc78a29adfa899249
Fatih Yıldırım <fatihyldrm3693@gmail.com>
2023-09-08 15:29:15 +0300

    first commit

--- Core/Domain/Entities/User.cs
+++ Core/Domain/Entities/User.cs
@@ -8,5 +8,11 @@ namespace Domain.Entities
 {
     public class User
     {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
     }
 }
